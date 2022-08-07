b51b996f74ca517311e52113d2379f26f6f123b1
Alex Dedov <q.wwwww.q@inbox.ru>
2022-08-07 23:21:41 +0700

    Project

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите оператор +/-/*/: или end для выхода");
+    string Operator = Console.ReadLine();
+    if (Operator == "end")
+    {
+        break;
+    }
+    Console.WriteLine("Введите число 1 ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число 2 ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    if (Operator == "+")
+    {
+        Console.WriteLine("Мы сложили числа");
+        Console.WriteLine(number1 + number2);
+    }
+    else if (Operator == "-")
+    {
+        Console.WriteLine("Мы вычли числа");
+        Console.WriteLine(number1 - number2);
+    }
+    else if (Operator == "*")
+    {
+        Console.WriteLine("Мы умножили числа");
+        Console.WriteLine(number1 * number2);
+    }
+    else if (Operator == ":")
+    {
+        Console.WriteLine("Мы разделили числа");
+        Console.WriteLine(number1 / number2);
+    }
+    
+    
+}
+
+
+
+
